b1cac11edf7d460bfc082cf095f04e4196bf55ab
Tiakiana <takana@exelion.dk>
2016-09-28 12:31:00 +0200

    Command and DataMapper Added Command is lazyly not finished

---
+++ CommandPattern/CommandPattern/TvOn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    class TvOn : Command
+    {
+        private ElectronicDevice electronic;
+
+        public TvOn(ElectronicDevice electronicDevice)
+        {
+            electronic = electronicDevice;
+        }
+
+        public void Execute()
+        {
+           electronic.On();
+         
+        }
+    }
+}

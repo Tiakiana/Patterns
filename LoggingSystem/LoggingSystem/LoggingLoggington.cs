44040de7c79ca122894c96d8143a8b1d28e8ac5a
Tiakiana <takana@exelion.dk>
2016-10-04 11:45:57 +0200

    LoggingSystem
    Loggingsystem Lavet

---
+++ LoggingSystem/LoggingSystem/LoggingLoggington.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoggingSystem
+{
+    class LoggingLoggington
+    {
+
+        public void WriteThisHereThing() {
+            Console.WriteLine("I am of writing this here thing");
+            LogWriter lw = new LogWriter("LoggingLoggington, Thing");
+
+        }
+
+        public void WriteThisHereThang()
+        {
+            Console.WriteLine("I am of writing this here thang");
+            LogWriter lw = new LogWriter("LoggingLoggington, Thang");
+        }
+
+    }
+}
---
+++ LoggingSystem/LoggingSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoggingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LoggingLoggington lg = new LoggingLoggington();
+            lg.WriteThisHereThang();
+            lg.WriteThisHereThing();
+            Console.ReadLine();
+
+        }
+    }
+}
